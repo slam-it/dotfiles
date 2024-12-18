09267c46aaefada79cf0e6605ec6280c9d8c1879
Sander Lammertink <sanderlammertink@gmail.com>
2024-12-18 22:19:13 +0100

    added zsh

---
+++ zsh/.config/zsh/alias.zsh
@@ -0,0 +1,17 @@
+[ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+
+alias zs="source ~/.zshrc"
+
+alias la="eza --all --icons=auto"
+alias ls="eza --icons=auto"
+alias ll="eza --long --icons=auto"
+alias tree="eza --tree --icons=auto"
+
+alias vi="nvim"
+
+alias y="yazi"
+alias c="clear"
+
+alias vdhoef="ssh slam-it@5.157.81.224"
+
+
---
+++ zsh/.config/zsh/plugins.zsh
@@ -0,0 +1,7 @@
+source $(brew --prefix)/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+source $(brew --prefix)/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
